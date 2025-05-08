65cbfd90e5eacc89b823f9af44c52d11765cee5f
Candecano <candecano11@gmail.com>
2025-05-08 13:00:59 -0300

    organizacion del proyecto

--- backend/src/mesa.ts
+++ backend/src/mesa.ts
@@ -72,30 +72,8 @@ console.error("No se pudo enviar la subcripcion:", err);
 res.status(200).json({ message: `Notificación enviada al profesor ${Idprofe}` });
 });
 
-//para probar
-router.post("/enviar-notificacion", (req: Request, res: Response) => {
-  const { title, body } = req.body;
-
-  const payload = JSON.stringify({
-    title: title || "Notificación de prueba",
-    body: body || "Esta es una notificación de prueba enviada desde el backend.",
-  });
-
-  subscripciones.forEach((subcripcion) => {
-    webPush.sendNotification(subcripcion, payload).catch((err) => {
-      console.error("Error al enviar la notificación:", err);
-    });
-  });
-
-  res.status(200).json({ message: "Notificación enviada a todos los suscriptores." });
-});
-
-
-
-
-
 export default router;
 
 
 
-//para probar
+
