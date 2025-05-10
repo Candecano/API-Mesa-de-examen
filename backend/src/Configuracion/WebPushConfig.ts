d5eabc94fd321393d590ef0ef09a8458f059aa46
Candecano <candecano11@gmail.com>
2025-05-10 12:51:42 -0300

    implementacion patron Singleton+organizacion proyecto

---
+++ backend/src/Configuracion/WebPushConfig.ts
@@ -0,0 +1,30 @@
+//Singleton-Patron de una unica instancia
+import webPush from "web-push";
+class WebPushConfig{
+private static instance: WebPushConfig;
+private constructor (){
+    webPush.setVapidDetails(
+        "mailto:tu-email@example.com",
+        process.env.CLAVE_PUBLICA || "", 
+        process.env.CLAVE_PRIVADA || ""
+    );
+
+
+}
+
+//metodo para devolver misma instancia
+public static getinstance(): WebPushConfig {
+
+if (!WebPushConfig.instance){
+    WebPushConfig.instance= new WebPushConfig();
+}
+return WebPushConfig.instance;
+
+}
+  public getWebPush() {
+    return webPush;
+  }
+
+}
+
+export default WebPushConfig;
