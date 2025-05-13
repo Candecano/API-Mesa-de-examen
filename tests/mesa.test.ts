22338969059b0f11bc636bc125822376f6671bc5
Lucas8276 <lucasnahuquin@gmail.com>
2025-05-12 21:52:04 -0300

    cambito

--- tests/mesa.test.ts
+++ tests/mesa.test.ts
@@ -2,7 +2,7 @@
 
 import request from "supertest";
 import express from "express";
-import rutas from "../backend/rutas"; 
+import rutas from "../backend/src/rutas"; 
 
 const app = express();
 app.use(express.json());
